c75ab7daa4a5e95531978c96d7d2e30c5e0d3da4
Nir Levy <nir.l@taboola.com>
2023-08-11 21:11:24 +0300

    skeleton for server with spring boot and ui

---
+++ classrooms-server/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("org.springframework.boot") version "2.7.12"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("plugin.spring") version "1.8.21"
+}
+
+group = "com.nirlevy"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":classrooms-engine"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.6") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty:2.7.6")
+    runtimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.8.22")
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.6")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
---
+++ classrooms-server/src/main/kotlin/com/nirlevy/classrooms/model/StudentDto.kt
@@ -0,0 +1,18 @@
+package com.nirlevy.classrooms.model
+
+import com.nirlevy.classrooms.data.Gender
+import com.nirlevy.classrooms.data.Grade
+
+data class StudentDto(
+    val name: String,
+    val school: String,
+    val gender: Gender,
+    val academicPerformance: Grade,
+    val behavioralPerformance: Grade,
+    val comments: String,
+    val friend1: String,
+    val friend2: String,
+    val friend3: String,
+    val friend4: String,
+    val notWith: String
+)
