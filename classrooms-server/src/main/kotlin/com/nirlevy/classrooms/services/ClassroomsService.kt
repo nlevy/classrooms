f3e79c844d3739e48dd9a6c52a2e366bd5934209
Nir Levy <nir.l@taboola.com>
2023-09-16 19:58:15 +0300

    add loggers

--- classrooms-server/src/main/kotlin/com/nirlevy/classrooms/services/ClassroomsService.kt
+++ classrooms-server/src/main/kotlin/com/nirlevy/classrooms/services/ClassroomsService.kt
@@ -5,6 +5,8 @@ import com.nirlevy.classrooms.model.ClassroomsDto
 import com.nirlevy.classrooms.model.StudentDto
 import com.nirlevy.classrooms.transformers.ClassroomsTransformer
 import com.nirlevy.classrooms.transformers.StudentTransformer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,10 +15,13 @@ class ClassroomsService(
     private val studentTransformer: StudentTransformer,
     private val classroomsEngine: ClassroomsEngine
 ) {
+    private val logger: Logger = LogManager.getLogger(ClassroomsService::class.java)
 
     fun buildClasses(students: List<StudentDto>, classesNumber: Int): ClassroomsDto {
+        logger.debug("Building classes for ${students.size} students, classes number: $classesNumber")
         val studentsList = studentTransformer.toStudentsList(students)
         val classes = classroomsEngine.buildClasses(studentsList, classesNumber)
+        logger.debug("Finished building classes. summary: {}", classes.classesSummaries)
         return classroomsTransformer.toClassroomsDto(classes)
     }
 
