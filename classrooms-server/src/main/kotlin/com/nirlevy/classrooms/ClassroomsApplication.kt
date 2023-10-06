e683712cb0578b5419d5a0ae22fd8f0190255aa3
Nir Levy <nir.l@taboola.com>
2023-10-06 16:49:01 +0300

    configurable urls for prod/dev

--- classrooms-server/src/main/kotlin/com/nirlevy/classrooms/ClassroomsApplication.kt
+++ classrooms-server/src/main/kotlin/com/nirlevy/classrooms/ClassroomsApplication.kt
@@ -1,9 +1,14 @@
 package com.nirlevy.classrooms
 
+import org.apache.logging.log4j.util.Strings
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class ClassroomsApplication
@@ -13,7 +18,17 @@ fun main(args: Array<String>) {
 }
 
 @Configuration
-class BeanConfig {
+class BeanConfig : WebMvcConfigurer {
+
+	@Value("\${cors.origins.default}")
+	private val defaultCorsOrigin: String = Strings.EMPTY
+
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**")
+			.allowedOrigins(defaultCorsOrigin)
+			.allowedMethods(HttpMethod.GET.name, HttpMethod.POST.name)
+			.allowedHeaders("*")
+	}
 
 	@Bean
 	fun classroomsEngine(): ClassroomsEngine {
--- classrooms-server/src/main/kotlin/com/nirlevy/classrooms/controllers/ClassroomsController.kt
+++ classrooms-server/src/main/kotlin/com/nirlevy/classrooms/controllers/ClassroomsController.kt
@@ -4,12 +4,14 @@ import com.nirlevy.classrooms.model.ClassroomsDto
 import com.nirlevy.classrooms.model.StudentDto
 import com.nirlevy.classrooms.services.ClassroomsService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ClassroomsController(private val classroomsService: ClassroomsService) {
 
-    @CrossOrigin(origins = ["http://localhost:5173","localhost:5173"])// TODO move to somewhere central, make configurable
     @PostMapping("/classrooms", consumes = ["application/json"], produces = ["application/json"])
     fun buildClassrooms(@RequestBody students: List<StudentDto>, @RequestParam classesNumber: Int) : ResponseEntity<ClassroomsDto> {
         val classes = classroomsService.buildClasses(students, classesNumber)
